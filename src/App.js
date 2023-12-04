98f8d34e791d5d4e93610a641a44f3bd2d7a8da8
Nilesh Kode <nilesh.kode89@gmail.com>
2023-12-04 22:51:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-800">
+      Hi User, Lets make netflix GPT
+    </div>
+  );
+}
+
+export default App;
